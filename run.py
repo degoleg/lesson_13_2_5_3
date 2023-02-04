d9210b863f738744420d9187cd175136aa5ebb64
Начальник.Смены <degoleg@yandex.ru>
2023-02-04 13:26:32 +0400

    all

---
+++ run.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.main.views import main_blueprint
+from app.books.views import books_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(main_blueprint)
+app.register_blueprint(books_blueprint)
+
+if __name__ == "__main__":
+    app.run()
